42605a8eee9dd1d15967d1c853da3db9d2846ce6
ArhangelM <prog.cpp.cs@gmail.com>
2025-09-28 13:46:01 +0300

    New builder and fix namespase

---
+++ Builder/Builders/INewBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Builder.Product.Engines;
+
+namespace NaiveDirector.Builders
+{
+    internal interface INewBuilder
+    {
+        INewBuilder Reset();
+        INewBuilder SetSeats(int number);
+        INewBuilder SetEngine(IEngine engine);
+        INewBuilder SetTripComputer(bool hasComputer);
+        INewBuilder SetGPS(bool hasGPS);
+    }
+}
